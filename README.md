7deca1c29ae7d59c2e5d9d83462ce4f779479460
KirisameVanilla <vanillaaaajp@gmail.com>
2024-12-24 17:29:02 +0800

    chinese CHARACTERS and README

---
+++ README.md
@@ -0,0 +1,14 @@
+## TaikoScorePicGenerator
+
+### USAGE
+
+```
+git clone https://github.com/KirisameVanilla/TaikoScorePicGenerator.git
+cd TaikoScorePicGenerator
+pip install -r requirements.txt
+```
+
+run `generate.py`
+
+### EXAMPLE
+![](res/example_result.png)
--- generate.py
+++ generate.py
@@ -10,9 +10,12 @@ def draw_small_pic(small_img_path, pic, position, scale = 1):
     pic.paste(img, position, img)  # 小图需要带透明度通道（RGBA）
 
 
-def draw_text(text, pic, size, position):
+def draw_text(text, pic, size, position, font_type):
     draw = ImageDraw.Draw(pic)
-    font = ImageFont.truetype('res/J002-A-OTF-KanteiryuStd-Ultra.otf', size)
+    if (font_type == "日文字库"):
+        font = ImageFont.truetype('res/J002-A-OTF-KanteiryuStd-Ultra.otf', size)
+    else:
+        font = ImageFont.truetype('res/AaKanTingLiu-2.ttf', size)
 
     outline_width = 3
     x, y = position
@@ -57,10 +60,11 @@ def generate_image():
         score_type = combo_score_type.get()
         difficulty = combo_difficulty.get()
         crown = combo_crown.get()
+        font_type = combo_font.get()
 
         background = Image.open('res/background.png')
-        background = draw_text(text1, background, 120, (250, 150))
-        background = draw_text(text2, background, 120, (250, 300))
+        background = draw_text(text1, background, 120, (250, 150), font_type)
+        background = draw_text(text2, background, 120, (250, 300), font_type)
         draw_score_rank(background, score_type, scale=2)
         draw_small_taiko(background, scale=2)
         draw_difficulty(background, difficulty, scale=2)
@@ -78,36 +82,41 @@ if __name__ == '__main__':
     root.title("太鼓成绩图生成器")
 
     # 输入文本1
-    tk.Label(root, text="曲名(日文)").grid(row=0, column=0, padx=10, pady=5)
+    tk.Label(root, text="曲名").grid(row=0, column=0, padx=10, pady=5)
     entry_text1 = tk.Entry(root, width=30)
     entry_text1.grid(row=0, column=1, padx=10, pady=5)
 
+    tk.Label(root, text="字库").grid(row=1, column=0, padx=10, pady=5)
+    combo_font = Combobox(root, values=["日文字库", "中文字库"], state="readonly", width=28)
+    combo_font.set("日文字库")  # 默认值
+    combo_font.grid(row=1, column=1, padx=10, pady=5)
+
     # 输入文本2
-    tk.Label(root, text="分数").grid(row=1, column=0, padx=10, pady=5)
+    tk.Label(root, text="分数").grid(row=2, column=0, padx=10, pady=5)
     entry_text2 = tk.Entry(root, width=30)
-    entry_text2.grid(row=1, column=1, padx=10, pady=5)
+    entry_text2.grid(row=2, column=1, padx=10, pady=5)
 
     # 输入分数类型
-    tk.Label(root, text="分数类型").grid(row=2, column=0, padx=10, pady=5)
+    tk.Label(root, text="分数类型").grid(row=3, column=0, padx=10, pady=5)
     combo_score_type = Combobox(root, values=["粉雅", "紫雅", "极"], state="readonly", width=28)
     combo_score_type.set("粉雅")  # 默认值
-    combo_score_type.grid(row=2, column=1, padx=10, pady=5)
+    combo_score_type.grid(row=3, column=1, padx=10, pady=5)
 
     # 难度
-    tk.Label(root, text="难度").grid(row=3, column=0, padx=10, pady=5)
+    tk.Label(root, text="难度").grid(row=4, column=0, padx=10, pady=5)
     combo_difficulty = Combobox(root, values=["鬼", "里鬼"], state="readonly", width=28)
     combo_difficulty.set("鬼")  # 默认值
-    combo_difficulty.grid(row=3, column=1, padx=10, pady=5)
+    combo_difficulty.grid(row=4, column=1, padx=10, pady=5)
 
     # 皇冠
-    tk.Label(root, text="皇冠").grid(row=4, column=0, padx=10, pady=5)
+    tk.Label(root, text="皇冠").grid(row=5, column=0, padx=10, pady=5)
     combo_crown = Combobox(root, values=["通关", "全连"], state="readonly", width=28)
     combo_crown.set("通关")  # 默认值
-    combo_crown.grid(row=4, column=1, padx=10, pady=5)
+    combo_crown.grid(row=5, column=1, padx=10, pady=5)
 
     # 生成图片按钮
     btn_generate = tk.Button(root, text="生成图片", command=generate_image)
-    btn_generate.grid(row=5, column=0, columnspan=2, pady=20)
+    btn_generate.grid(row=6, column=0, columnspan=2, pady=20)
 
     # 运行主循环
     root.mainloop()
